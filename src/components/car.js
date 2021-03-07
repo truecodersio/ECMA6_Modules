beb3aa7e4d7a6b1a3ca525987f0a30706746b78c
Ben <bbryant@truecoders.io>
2021-03-07 09:10:26 -0600

    Added solution for exercise

--- src/components/car.js
+++ src/components/car.js
@@ -1,11 +1,9 @@
-export class Car {
+class Car {
   constructor(make, model, year) {
     this.make = make;
     this.model = model;
     this.year = year;
   }
-
-  info() {
-    return (`This ${make} ${model} was made in ${year}.`);
-  }
 }
+
+export default Car;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import WishList from "./components/wishList";
+import Car from "./components/car";
+import { displayCarInfo } from "./utils/DOMMethods";
+
+window.onload = function () {
+  const form = document.getElementById("submitForm");
+  const makeInput = document.getElementById("makeInput");
+  const modelInput = document.getElementById("modelInput");
+  const yearInput = document.getElementById("yearInput");
+
+  const wishList = new WishList();
+
+  const [removeBtn] = document.getElementsByClassName("removeBtn");
+  removeBtn.addEventListener("click", function () {
+    wishList.remove(this.id);
+    displayCarInfo({ make: "", model: "", year: "" });
+  });
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let newCar = new Car(makeInput.value, modelInput.value, yearInput.value);
+    wishList.add(newCar);
+  });
+};
